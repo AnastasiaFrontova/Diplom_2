90a791e826b41268841d89c462d5f4702a483a01
Anastasia Frontova <anastasia.frontova@yandex.ru>
2025-06-27 14:20:03 +0300

    Update .gitignore: exclude .idea, __pycache__, and other temp files

---
+++ data/response.py
@@ -0,0 +1,4 @@
+class Response:  # Тексты ответов сервера
+    CREATE_USER_DOUBLE = "User already exists"
+    INTERNAL_SERVER_ERROR = 'Internal Server Error'
+    RESPONSE_NOT_AUTHORIZED = 'You should be authorised'
---
+++ data/url.py
@@ -0,0 +1,4 @@
+
+    class URL:
+        # Базовый URL сервиса Stellar Burgers
+        main_url = 'https://stellarburgers.nomoreparties.site'
